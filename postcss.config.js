798cd392cc472d602c204d5057bfbf399c4bb993
yeondon <s25030@gsm.hs.kr>
2025-08-12 06:32:10 +0900

    기봇 세팅

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {}, // v4에서는 이렇게 써야 함
+    autoprefixer: {},
+  },
+};
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./style.css"; // Tailwind 불러오기
+
+createApp(App).mount("#app");
